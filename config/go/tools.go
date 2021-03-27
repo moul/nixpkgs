e6a3fb466a514d69df01e4f9653d3ac53a2f9646
Manfred Touron <94029+moul@users.noreply.github.com>
2021-03-27 00:12:58 +0000

    feat: add gci
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- config/go/tools.go
+++ config/go/tools.go
@@ -1,6 +1,7 @@
 package tools
 
 import (
+	_ "github.com/daixiang0/gci/pkg/gci"
 	_ "github.com/goreleaser/goreleaser/pkg/build"
 	_ "github.com/maruel/panicparse/v2/stack"
 	_ "golang.org/x/tools/cover"
