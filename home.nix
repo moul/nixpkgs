eecb63a0408e9849d7949f0f96002157e53141c8
Manfred Touron <94029+moul@users.noreply.github.com>
2021-04-07 00:10:09 +0200

    feat: configure language

--- home.nix
+++ home.nix
@@ -13,6 +13,20 @@ in
     };
   };
   home = {
+    language = {
+      base = "en_US.UTF-8";
+      address = "en_US.UTF-8";
+      collate = "en_US.UTF-8";
+      ctype = "en_US.UTF-8";
+      measurement = "en_US.UTF-8";
+      messages = "en_US.UTF-8";
+      monetary = "en_US.UTF-8";
+      name = "en_US.UTF-8";
+      numeric = "en_US.UTF-8";
+      paper = "en_US.UTF-8";
+      telephone = "en_US.UTF-8";
+      time = "en_US.UTF-8";
+    };
     packages = with pkgs; [
       ascii
       aspellDicts.en
