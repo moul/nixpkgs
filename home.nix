2bec04654fd36d22c391545d1d23c64028c29564
Manfred Touron <94029+moul@users.noreply.github.com>
2021-04-17 02:46:09 +0200

    chore: fix locale

--- home.nix
+++ home.nix
@@ -11,18 +11,18 @@ in
   };
 
   home.language = (if pkgs.stdenv.isDarwin then {
-    base = "en-US.UTF-8";
-    address = "en-US.UTF-8";
-    collate = "en-US.UTF-8";
-    ctype = "en-US.UTF-8";
-    measurement = "en-US.UTF-8";
-    messages = "en-US.UTF-8";
-    monetary = "en-US.UTF-8";
-    name = "en-US.UTF-8";
-    numeric = "en-US.UTF-8";
-    paper = "en-US.UTF-8";
-    telephone = "en-US.UTF-8";
-    time = "en-US.UTF-8";
+    base = "en_US.UTF-8";
+    address = "en_US.UTF-8";
+    collate = "en_US.UTF-8";
+    ctype = "en_US.UTF-8";
+    measurement = "en_US.UTF-8";
+    messages = "en_US.UTF-8";
+    monetary = "en_US.UTF-8";
+    name = "en_US.UTF-8";
+    numeric = "en_US.UTF-8";
+    paper = "en_US.UTF-8";
+    telephone = "en_US.UTF-8";
+    time = "en_US.UTF-8";
   } else {
     base = "C.UTF-8";
     address = "C.UTF-8";
