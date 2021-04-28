ea38f374fddb9f1e5c9ebd3e84ecfab3f098cf37
Manfred Touron <94029+moul@users.noreply.github.com>
2021-04-28 18:51:15 +0000

    chore: improve readability of post install hook

--- home.nix
+++ home.nix
@@ -127,23 +127,30 @@ in {
       "onFilesChange"
       "installPackages"
     ] ''
-      $DRY_RUN_CMD ln -sf $VERBOSE_ARG ${configd}/.spacemacs ~/.spacemacs;
-      mkdir -p ~/.ssh;
-      $DRY_RUN_CMD ln -sf $VERBOSE_ARG ${configd}/assh.yml ~/.ssh/assh.yml;
+      # SSH
+      ln -sf ${configd}/assh.yml ~/.ssh/assh.yml;
       assh config build > ~/.ssh/config;
-      # FIXME: replace with niv/go2nix
-      pushd ~/.config/nixpkgs/config/go
       chmod 711 ~/.ssh
       chmod 600 ~/.ssh/config || true
+
+      # SPACEMACS
+      ln -sf ${configd}/.spacemacs ~/.spacemacs;
+      mkdir -p ~/.ssh;
       if [ ! -d ~/.emacs.d/.git ]; then
         rm -rf ~/.emacs.d.old
         mv ~/.emacs.d ~/.emacs.d.old || true
         git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
       fi
+
+      # GO
+      # FIXME: replace with niv/go2nix
+      pushd ~/.config/nixpkgs/config/go
       if [ "$HOME/.nix-profile/bin/go" = "$(which go)" ]; then
         GO="${pkgs.go}/bin/go" make install || true # sometimes it fails because we miss some env vars
       fi
       popd
+
+      # NPM
       mkdir -p ~/.npm-global
       npm config set prefix ~/.npm-global
       npm i -g tern prettier js-beautify eslint babel-eslint eslint-plugin-react
