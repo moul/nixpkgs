5547bd253a27c366a29bc3b2914536c165a9c6e2
Manfred Touron <94029+moul@users.noreply.github.com>
2021-06-27 12:19:31 +0200

    chore: update cachix bucket list

--- darwin/bootstrap.nix
+++ darwin/bootstrap.nix
@@ -8,12 +8,14 @@
     "https://iohk.cachix.org"
     "https://hydra.iohk.io"
     "https://gfanton.cachix.org"
+    "https://moul.cachix.org"
   ];
   nix.binaryCachePublicKeys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
     "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
     "gfanton.cachix.org-1:i8zC+UjhhW5Wx2iRibhexJeBb1jOU/8oRFGG60IaAmI="
+    "moul.cachix.org-1:jcmTECmIfe9zam+p4sP3RhEXmH7QTTChd9ax/vo1CYs="
   ];
   nix.trustedUsers = [ "@admin" ];
   users.nix.configureBuildUsers = true;
