68ae4200a385cf1e2f29608280eb66dd46961b87
Manfred Touron <94029+moul@users.noreply.github.com>
2022-09-28 13:09:47 +0000

    chore: conditional 1password
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- home/git.nix
+++ home/git.nix
@@ -10,12 +10,13 @@
     # core.editor = "${pkgs.emacs-nox}/bin/emacs --remote-wait-silent -cc split";
     diff.colorMoved = "default";
     pull.rebase = true;
-    user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID+r9TcD4g/DVW5/9W9grjD700PJccMonLEWnB+v++42";
-    commit.gpgsign = true;
-    gpg.format = "ssh";
-    gpg.ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
     init.defaultBranch = "main";
-  };
+  } // (if pkgs.stdenv.isDarwin then {
+    gpg.format = "ssh";
+    user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID+r9TcD4g/DVW5/9W9grjD700PJccMonLEWnB+v++42";
+    gpg.ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+    commit.gpgsign = true;
+  } else {});
 
   programs.git.ignores = [ ".DS_Store" ];
 
