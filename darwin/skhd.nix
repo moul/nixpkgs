5f660fb46bba5a6ac29277118b1a98d3b9c2fa6e
moul <94029+moul@users.noreply.github.com>
2023-09-28 18:46:27 -0400

    chore: update skhd config
    Signed-off-by: moul <94029+moul@users.noreply.github.com>

--- darwin/skhd.nix
+++ darwin/skhd.nix
@@ -20,39 +20,35 @@ in {
       ## Window command
 
       # focus
-      lcmd + ctrl - left  : ${yabai} -m window --focus west  || yabai -m display --focus west
-      lcmd + ctrl - down  : ${yabai} -m window --focus south || yabai -m display --focus south
-      lcmd + ctrl - up    : ${yabai} -m window --focus north || yabai -m display --focus north
-      lcmd + ctrl - right : ${yabai} -m window --focus east  || yabai -m display --focus east
+      alt - left  : ${yabai} -m window --focus west  || yabai -m display --focus west
+      alt - down  : ${yabai} -m window --focus south || yabai -m display --focus south
+      alt - up    : ${yabai} -m window --focus north || yabai -m display --focus north
+      alt - right : ${yabai} -m window --focus east  || yabai -m display --focus east
+      alt - j     : ${yabai} -m window --focus west  || yabai -m display --focus west
+      alt - n     : ${yabai} -m window --focus south || yabai -m display --focus south
+      alt - p     : ${yabai} -m window --focus north || yabai -m display --focus north
+      alt - k     : ${yabai} -m window --focus east  || yabai -m display --focus east
 
       # swap
-      lcmd + shift - left : ${yabai} -m window --swap west \
-           || (${yabai} -m window --display west && yabai -m display --focus west) \
-           || ${yabai} -m window --toggle split
-      lcmd + shift - right : ${yabai} -m window --swap east \
-           || (${yabai} -m window --display east && yabai -m display --focus east) \
-           || ${yabai} -m window --toggle split
-      lcmd + shift - up : ${yabai} -m window --swap north \
-           || (${yabai} -m window --display north && yabai -m display --focus north) \
-           || ${yabai} -m window --toggle split
-      lcmd + shift - down : ${yabai} -m window --swap south \
-           || (${yabai} -m window --display south && yabai -m display --focus south) \
-           || ${yabai} -m window --toggle split
+      alt + shift - left : ${yabai} -m window --swap west  || (${yabai} -m window --display west && yabai -m display --focus west)   || ${yabai} -m window --toggle split
+      alt + shift - right : ${yabai} -m window --swap east || (${yabai} -m window --display east && yabai -m display --focus east)   || ${yabai} -m window --toggle split
+      alt + shift - up : ${yabai} -m window --swap north   || (${yabai} -m window --display north && yabai -m display --focus north) || ${yabai} -m window --toggle split
+      alt + shift - down : ${yabai} -m window --swap south || (${yabai} -m window --display south && yabai -m display --focus south) || ${yabai} -m window --toggle split
 
       # split
-      lcmd + shift - d : ${yabai} -m window --toggle split
+      #lcmd + shift - d : ${yabai} -m window --toggle split
 
       # increase window size
-      ctrl + lcmd + shift - left  : yabai -m window --resize left:-40:0
-      ctrl + lcmd + shift - down  : yabai -m window --resize bottom:0:40
-      ctrl + lcmd + shift - up    : yabai -m window --resize top:0:-40
-      ctrl + lcmd + shift - right : yabai -m window --resize right:40:0
+      #ctrl + lcmd + shift - left  : yabai -m window --resize left:-40:0
+      #ctrl + lcmd + shift - down  : yabai -m window --resize bottom:0:40
+      #ctrl + lcmd + shift - up    : yabai -m window --resize top:0:-40
+      #ctrl + lcmd + shift - right : yabai -m window --resize right:40:0
 
       # decrease window size
-      ctrl + alt + shift - left  : yabai -m window --resize left:40:0
-      ctrl + alt + shift - down  : yabai -m window --resize bottom:0:-40
-      ctrl + alt + shift - up    : yabai -m window --resize top:0:40
-      ctrl + alt + shift - right : yabai -m window --resize right:-40:0
+      #ctrl + alt + shift - left  : yabai -m window --resize left:40:0
+      #ctrl + alt + shift - down  : yabai -m window --resize bottom:0:-40
+      #ctrl + alt + shift - up    : yabai -m window --resize top:0:40
+      #ctrl + alt + shift - right : yabai -m window --resize right:-40:0
 
       # warp
       # alt + shift - left : ${yabai} -m window --warp west || ${yabai} -m window --toggle split
@@ -61,40 +57,30 @@ in {
       # alt + shift - right : ${yabai} -m window --warp east || ${yabai} -m window --toggle split
 
       # fullscreen
-      ctrl + lcmd + shift - f : ${yabai} -m window --toggle native-fullscreen
-      ctrl + lcmd - f : ${yabai} -m window --focus mouse && \
-                ${yabai} -m window --toggle zoom-fullscreen
-      ctrl + lcmd - p : ${yabai} -m window --focus mouse && \
-              ${yabai} -m window --toggle zoom-parent
-      ctrl + lcmd - m : ${yabai} -m window --minimize
-
-
-      # equalize window
-      ctrl + lcmd - e : ${yabai} -m space --balance
-
-      # mirror x and y
-      ctrl + lcmd - x: ${yabai} -m space --mirror x-axis
-      ctrl + lcmd - y: ${yabai} -m space --mirror y-axis
+      #alt - f : ${yabai} -m window --focus mouse && ${yabai} -m window --toggle zoom-fullscreen
+      alt - f : ${yabai} -m window --toggle zoom-fullscreen
+      ctrl + lcmd - f : ${yabai} -m window --toggle native-fullscreen
+      #ctrl + lcmd - p : ${yabai} -m window --focus mouse && ${yabai} -m window --toggle zoom-parent
+      #ctrl + lcmd - m : ${yabai} -m window --minimize
 
       # toggle bsp
-      ctrl + lcmd + shift - b : ${yabai} -m window --toggle float || ${yabai} -m window --toggle bsp
+      alt - b : ${yabai} -m window --toggle float || ${yabai} -m window --toggle bsp
 
       # toggle split
-      ctrl + lcmd - s : ${yabai} -m window --toggle split
+      #ctrl + lcmd - s : ${yabai} -m window --toggle split
 
       # emacs client
       # @TODO: use nix abs path
       # @TODO: use current term directory
       # alt + shift - o : emacsclient --create-frame
 
-
       # focus next space, or the first space if we are at the end
-      lcmd + ctrl - z : ${yabai} -m space --focus next || ${yabai} -m space --focus first
+      #lcmd + ctrl - z : ${yabai} -m space --focus next || ${yabai} -m space --focus first
 
       # focus prev space, or the last space if we are at the beginning
-      lcmd + ctrl - c : ${yabai} -m space --focus prev || ${yabai} -m space --focus last
+      #lcmd + ctrl - c : ${yabai} -m space --focus prev || ${yabai} -m space --focus last
 
-      alt + ctrl - left : ${skhd} -k "cmd + ctrl + alt - left"
+      alt + ctrl - left :  ${skhd} -k "cmd + ctrl + alt - left"
       alt + ctrl - right : ${skhd} -k "cmd + ctrl + alt - right"
       alt + ctrl - 1 : ${skhd} -k "cmd + ctrl + alt - 1"
       alt + ctrl - 2 : ${skhd} -k "cmd + ctrl + alt - 2"
