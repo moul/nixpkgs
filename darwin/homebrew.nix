e2b50ae38b2b173419dc3c0e025c8493f8151559
moul <94029+moul@users.noreply.github.com>
2024-05-30 17:50:55 +0200

    chore: add 1password
    Signed-off-by: moul <94029+moul@users.noreply.github.com>

--- darwin/homebrew.nix
+++ darwin/homebrew.nix
@@ -81,7 +81,7 @@ in {
   # If an app isn't available in the Mac App Store, or the version in the App Store has
   # limitiations, e.g., Transmit, install the Homebrew Cask.
   homebrew.casks = [
-    #"1password"
+    "1password"
     #"1password-cli"
     #"backblaze"
     #"blobby-volley2"
