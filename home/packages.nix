8f5e3144bd5fe57bc9a6d7aa5fcb76cedfd5200b
moul <94029+moul@users.noreply.github.com>
2025-01-23 08:46:57 +0100

    chore: update emacs
    Signed-off-by: moul <94029+moul@users.noreply.github.com>

--- home/packages.nix
+++ home/packages.nix
@@ -162,6 +162,9 @@ in {
       unixtools.watch
       #youtube-dl
 
+      semgrep
+      golangci-lint-langserver
+      
       # Useful nix related tools
       vivid
       cachix # adding/managing alternative binary caches hosted by Cachix
